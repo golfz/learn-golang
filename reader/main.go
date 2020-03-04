0976351193c1e9a1bff4fbc9b9396cec7e6393b1
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-03-04 19:50:05 +0700

    read first 33 bytes

--- reader/main.go
+++ reader/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("sloth.txt")
+	if err != nil {
+		log.Fatalf("error opening sloth.txt: %v", err)
+	}
+	defer file.Close()
+
+	bytesRead := make([]byte, 33)
+
+	n, err := file.Read(bytesRead)
+	if err != nil {
+		log.Fatalf("error reading from sloth.txt: %v", err)
+	}
+
+	log.Printf("We read \"%s\" into bytesRead (%d bytes)", string(bytesRead), n)
 }
