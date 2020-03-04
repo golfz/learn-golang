cf0ba57d0e3370c815264229054e68c165e2c4d3
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-03-04 19:56:29 +0700

    read again

--- reader/main.go
+++ reader/main.go
@@ -20,4 +20,11 @@ func main() {
 	}
 
 	log.Printf("We read \"%s\" into bytesRead (%d bytes)", string(bytesRead), n)
+
+	n, err = file.Read(bytesRead)
+	if err != nil {
+		log.Fatalf("error reading from sloth.txt: %v", err)
+	}
+
+	log.Printf("We read \"%s\" into bytesRead (%d bytes)", string(bytesRead), n)
 }
