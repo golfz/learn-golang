3399ae2bfad090541f614960560fb8df1fa5ccfa
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-03-10 11:45:34 +0700

    add word_test.go

---
+++ test/word/word_test.go
@@ -0,0 +1,14 @@
+package word
+
+import (
+	"testing"
+)
+
+func TestPalindrome(t *testing.T) {
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated") = false`)
+	}
+	if !IsPalindrome("kayak") {
+		t.Error(`IsPalindrome("kayak") = false`)
+	}
+}
