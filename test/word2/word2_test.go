6146d7cde29293c3111767ae443224df81aa149c
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-03-10 20:47:59 +0700

    update test failure message to

--- test/word2/word2_test.go
+++ test/word2/word2_test.go
@@ -23,7 +23,7 @@ func TestIsPalindrome(t *testing.T) {
 	}
 	for _, test := range tests {
 		if got := IsPalindrome(test.input); got != test.want {
-			t.Errorf("IsPalindrome(%q) = %v", test.input, got)
+			t.Errorf("IsPalindrome(%q) = %v, want %v", test.input, got, test.want)
 		}
 	}
 }
