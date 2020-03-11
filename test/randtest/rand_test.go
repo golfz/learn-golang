325febd2ca8d3aa79ab1996c1e47b165e22c7eed
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-03-11 20:46:51 +0700

    add random test

---
+++ test/randtest/rand_test.go
@@ -0,0 +1,21 @@
+package randtest
+
+import (
+	word "github.com/golfz/learn-golang/test/word2"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := RandomPalindrome(rng)
+		if !word.IsPalindrome(p) {
+			t.Errorf("IsPanlindrome(%q) = false", p)
+		}
+	}
+}
