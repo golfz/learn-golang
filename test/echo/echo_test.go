8c8ae7f7a46a85dd6da1e9ea1152373d440f2aca
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-03-13 12:12:13 +0700

    add test loop

--- test/echo/echo_test.go
+++ test/echo/echo_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
 
 func TestEcho(t *testing.T) {
 	var tests = []struct {
@@ -14,5 +18,20 @@ func TestEcho(t *testing.T) {
 		{true, "\t", []string{"one", "two", "three"}, "one\ttwo\tthree\n"},
 		{true, ",", []string{"a", "b", "c"}, "a, b, c\n"},
 		{false, ":", []string{"1", "2", "3"}, "1:2:3"},
+	}
+
+	for _, test := range tests {
+		descr := fmt.Sprintf("echo(%v, %q, %q)", test.newline, test.sep, test.args)
+
+		out = new(bytes.Buffer) // captured output
+
+		if err := echo(test.newline, test.sep, test.args); err != nil {
+			t.Errorf("%s failed: %v", descr, err)
+			continue
+		}
+		got := out.(*bytes.Buffer).String()
+		if got != test.want {
+			t.Errorf("%s = %q, want %q", descr, got, test.want)
+		}
 	}
 }
