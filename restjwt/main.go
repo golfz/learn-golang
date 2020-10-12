2fe82929ef631553967b73040c61cb7160616ccb
Surattikorn <surattikorn@gmail.com>
2020-10-12 17:31:22 +0700

    add router in main

--- restjwt/main.go
+++ restjwt/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "log"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
-	log.Println("rest jwt")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(ProtectedEndpoint)).Methods("POST")
+
+	log.Println("Listen on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
