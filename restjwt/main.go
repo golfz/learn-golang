8944e10e01b4e59ba2b6b04c141153fdac530b00
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-10-13 21:36:46 +0700

    add model

--- restjwt/main.go
+++ restjwt/main.go
@@ -8,6 +8,20 @@ import (
 	"net/http"
 )
 
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWT struct {
+	Token string `json:"token"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	router := mux.NewRouter()
 
